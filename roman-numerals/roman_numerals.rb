03f52f4556602e24478c100d13e3ea7da40bd5b7
Atul Bhosale <atul1bhosale@gmail.com>
2016-04-16 12:27:20 +0530

    1st and 2nd test pass

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,10 +1,12 @@
 class Fixnum
 	def to_roman
-		roman_map = {"1" => "I"}
+		roman_map = {"1" => "I", "5" => "V", "10" => "X", "100" => "C", "1000" => "M"}
 		final_value = ""
 		final_value = case self
 		when 1
 			roman_map["1"]
+		when 2
+			roman_map["1"] + roman_map["1"]
 		end
 	end
 end
